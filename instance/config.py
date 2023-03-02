a12552f43a7686779d7487a916665b854ff2e51e
zeelrupapara <zeelrupapara@gmail.com>
2023-03-02 13:17:25 +0530

    feat: setup env

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,14 @@
-SECRET_KEY= 'secret key'
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://note:note@db:3306/note"
+import os
+
+db_host = os.environ.get('DB_HOST')
+db_port = os.environ.get('DB_PORT')
+db_name = os.environ.get('DB_NAME')
+db_username = os.environ.get('DB_USERNAME')
+db_password = os.environ.get('DB_PASSWORD')
+database_url = "mysql+pymysql://${}:${}@${}:${}/${}".format(
+    db_username, db_password, db_host, db_port, db_name)
+
+SECRET_KEY = 'secret key'
+SQLALCHEMY_DATABASE_URI = database_url
 ENV = 'development'
 DEBUG = True
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from website import create_app
+import os
 
+port = os.environ.get('PORT') | "5000"
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True,)
+    app.run(debug=True, port=port)
