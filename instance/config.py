6e3c3023cac3a05611ad4b3afd011f1e0dd74e2e
Ankit Jilka <ankit.jilka@luna.pride.improwised.dev>
2023-03-03 17:10:31 +0530

    kubernetes complete

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,14 @@
-SECRET_KEY= 'secret key'
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://note:note@db:3306/note"
+import os
+
+db_host = os.getenv('DB_HOST', default="db")
+db_port = os.getenv('DB_PORT', default="3306")
+db_name = os.getenv('DB_NAME', default="note")
+db_username = os.getenv('DB_USERNAME', default="note")
+db_password = os.getenv('DB_PASSWORD', default="note")
+database_url = "mysql+pymysql://{}:{}@{}:{}/{}".format(
+    db_username, db_password, db_host, db_port, db_name)
+
+SECRET_KEY = 'secret key'
+SQLALCHEMY_DATABASE_URI = database_url
 ENV = 'development'
 DEBUG = True
